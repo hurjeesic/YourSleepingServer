2685add8dd7dede0495882e28d5f2d42b5fa7771
Jimen <hurjeesic@naver.com>
2019-11-19 01:57:40 +0900

    template 작성, 데이터 리스트 페이지 생성

--- yoursleeping/views.py
+++ yoursleeping/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Activity
 
 # Create your views here.
 def index(request):
     return render(request, 'yoursleeping/index.html', {})
+
+def data_list(request):
+    datas = Activity.objects.filter().order_by('date').order_by('time')
+
+    return render(request, 'yoursleeping/data_list.html', { 'datas' : datas })
